89a0e5aec1317b9150812f2d1cce8bd18521bdb8
lawrencedulian <lawrenceedulian@gmail.com>
2022-12-20 22:45:50 +0100

    added faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,35 +2,27 @@
 
 namespace Database\Seeders;
 
-use App\Functions\Helpers;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(Faker $faker)
     {
-        $trainsData = Helpers::getCsvData(__DIR__ . '/trains.csv');
-        foreach ($trainsData as $key => $train) {
-            if ($key !== 0) {
-                $newTrain = new Train();
-                $newTrain->azienda = $train[0];
-                $newTrain->stazione_di_partenza = $train[1];
-                $newTrain->stazione_di_arrivo = $train[2];
-                $newTrain->orario_di_partenza = $train[3];
-                $newTrain->orario_di_arrivo = $train[4];
-                $newTrain->codice_treno = $train[5];
-                $newTrain->numero_carrozze = $train[6];
-                $newTrain->is_on_time = $train[7];
-                $newTrain->is_cancelled = $train[8];
-                $newTrain->save();
-            }
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->randomElement(['Trenitalia', 'FrecciaRossa', 'Italotreno']);
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
+            $newTrain->orario_di_partenza = $faker->dateTimeThisYear();
+            $newTrain->orario_di_arrivo = $faker->dateTimeThisYear();
+            $newTrain->codice_treno = $faker->randomNumber(7, true);
+            $newTrain->numero_carrozze = $faker->numberBetween(5, 20);
+            $newTrain->is_on_time = $faker->numberBetween(0, 1);
+            $newTrain->is_cancelled = $faker->numberBetween(0, 1);
+            $newTrain->save();
         }
     }
 }
